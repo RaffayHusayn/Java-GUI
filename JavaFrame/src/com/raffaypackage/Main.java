2cde1e1b48db3a1dea81977cf554e686d3733cb0
raffayhusayn <raffayhusayn@gmail.com>
2020-09-16 17:21:01 +0500

    Simple 2 Button GUI with event listeners

---
+++ JavaFrame/src/com/raffaypackage/Main.java
@@ -0,0 +1,15 @@
+package com.raffaypackage;
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Frame frame = new Frame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//      frame.setSize(300,300);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
